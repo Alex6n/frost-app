7a3c41831db2f2018fc58791f15d6e1b237dc137
Alex6n <hheikal.work@gmail.com>
2023-12-27 18:58:37 +0200

    Add social media icons to the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,9 @@ import { Separator } from "./ui/separator";
 import Link from "next/link";
 import { useRef } from "react";
 import Statistics from "./Statistics";
-
+import { Github, Twitter } from "lucide-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiscord } from "@fortawesome/free-brands-svg-icons";
 interface FooterProps {
   usefullLinks: {
     label: string;
@@ -27,10 +29,10 @@ const Footer = ({ usefullLinks, appData }: FooterProps) => {
   const footerRef = useRef(null);
 
   return (
-    <section ref={footerRef} className="w-screen md:h-96">
+    <footer ref={footerRef} className="w-screen md:h-96">
       <Separator className="bg-foreground/10" />
 
-      <div className="bg-gradient-to-br from-primary/40 to-secondary h-full mx-10 md:mx-24 mt-10 rounded-t-2xl flex flex-cols-3 justify-center">
+      <div className="bg-gradient-to-br from-primary/20 to-transparent h-full mx-10 md:mx-24 mt-10 rounded-t-2xl flex flex-cols-3 justify-center">
         <Statistics appData={appData} footerRef={footerRef} />
 
         <div className="col-span-1 -translate-y-24 mx-10">
@@ -38,6 +40,19 @@ const Footer = ({ usefullLinks, appData }: FooterProps) => {
           <p className="text-center font-semibold mt-10">
             © 2024 All rights reserved.
           </p>
+          <div className="flex flex-row justify-center gap-4 my-3">
+            <a href="#">
+              <Github className="w-5 h-5" />
+            </a>
+
+            <a href="#">
+              <FontAwesomeIcon icon={faDiscord} className="h-5 w-5" />
+            </a>
+
+            <a href="#">
+              <Twitter className="w-5 h-5" />
+            </a>
+          </div>
         </div>
 
         <div className="hidden lg:flex flex-cols-3 w-[500px] space-between space-x-20 my-auto mx-10">
@@ -55,7 +70,7 @@ const Footer = ({ usefullLinks, appData }: FooterProps) => {
           ))}
         </div>
       </div>
-    </section>
+    </footer>
   );
 };
 
