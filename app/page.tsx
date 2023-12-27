8a4acc11ada4a90de035700efa8c07b79316a3e0
Alex6n <hheikal.work@gmail.com>
2023-12-27 18:59:01 +0200

    Refactor page layout and section styling

--- app/page.tsx
+++ app/page.tsx
@@ -46,45 +46,49 @@ export default function Home() {
 
       <Features perks={perks} />
 
-      <Section>
-        <SectionDetails
-          label="Frost First"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      <section>
+        <Section>
+          <SectionDetails
+            label="Frost First"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
           molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
           numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga"
-        >
-          <Hop className="h-7 w-7 text-muted-foreground" />
-        </SectionDetails>
+          >
+            <Hop className="h-7 w-7 text-muted-foreground" />
+          </SectionDetails>
 
-        <div className="flex flex-col justify-center">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="https://assets.materialup.com/uploads/320be02a-aa8f-4376-8a35-5457274fb45b/attachment.png"
-            alt="Frost First"
-            className="aspect-video object-cover max-w-xl m-14 rounded-3xl shadow-lg shadow-black/60 border-zinc-100/20 border-2"
-          />
-        </div>
-      </Section>
+          <div className="flex flex-col justify-center">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://assets.materialup.com/uploads/320be02a-aa8f-4376-8a35-5457274fb45b/attachment.png"
+              alt="Frost First"
+              className="aspect-video object-cover max-w-xl lg:max-w-sm xl:max-w-xl m-14 rounded-3xl shadow-lg shadow-black/60 border-zinc-100/20 border-2"
+            />
+          </div>
+        </Section>
 
-      <Section>
-        <div className="flex flex-col justify-center">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="https://assets.materialup.com/uploads/320be02a-aa8f-4376-8a35-5457274fb45b/attachment.png"
-            alt="Frost Second"
-            className="aspect-video object-cover max-w-xl m-14 rounded-3xl shadow-lg shadow-black/60 border-zinc-100/20 border-2"
-          />
-        </div>
+        <Section>
+          <div className="flex flex-col justify-center">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://assets.materialup.com/uploads/320be02a-aa8f-4376-8a35-5457274fb45b/attachment.png"
+              alt="Frost Second"
+              className="aspect-video object-cover max-w-xl lg:max-w-sm xl:max-w-xl m-14 rounded-3xl shadow-lg shadow-black/60 border-zinc-100/20 border-2"
+            />
+          </div>
 
-        <SectionDetails
-          label="Frost Second"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+          <SectionDetails
+            label="Frost Second"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
           molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
           numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga"
-        >
-          <Coffee className="h-7 w-7 text-muted-foreground" />
-        </SectionDetails>
-      </Section>
+          >
+            <Coffee className="h-7 w-7 text-muted-foreground" />
+          </SectionDetails>
+        </Section>
+      </section>
+
+      {/* TODO: add top partners here */}
     </main>
   );
 }
--- components/Section.tsx
+++ components/Section.tsx
@@ -1,7 +1,7 @@
 const Section = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="w-screen md:h-96 my-36">
-      <div className="flex flex-cols-2 justify-center gap-28 h-full">
+    <section className="w-screen md:h-96 my-20 md:my-72 lg:my-36">
+      <div className="block lg:flex flex-cols-2 justify-center xl:gap-28 h-full">
         {children}
       </div>
     </section>
