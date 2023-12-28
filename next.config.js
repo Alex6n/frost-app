9ff00fec12e23ed361622771db4cf683ccd2ec7d
Alex6n <hheikal.work@gmail.com>
2023-12-28 23:11:43 +0200

    Add SVG loader and disable font optimization in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
-const { webpack } = require('next/dist/compiled/webpack/webpack')
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    optimizeFonts: false,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+    return config;
+  },
+};
 
-module.exports = {
-    webpack(config) {
-        config.module.rules.push(
-            {
-                test: /\.svg$/,
-                use: [{ loader: "@svgr/webpack", options: { icon: true } }],
-            },
-        )
-        return config
-    }
-}
+module.exports = nextConfig;
