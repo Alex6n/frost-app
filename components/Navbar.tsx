d98143909d7c2e64d8c08a98729e8ae9f6d41877
Hamza Heikal <hheikal.work@gmail.com>
2025-08-15 00:26:23 +0300

    add necessary package

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -96,7 +96,6 @@ export function Navbar() {
                 Status
               </Link>
             </NavigationMenuItem>
-            {/* TODO: Switch Language Menu NavItem */}
           </NavigationMenuList>
         </NavigationMenu>
 
