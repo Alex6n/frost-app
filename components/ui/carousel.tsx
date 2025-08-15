bba873c1df0bff5333732c774d54f6b84eeb0b7d
Hamza Heikal <hheikal.work@gmail.com>
2025-08-15 17:25:57 +0300

    refactor: update imports for CarouselOptions and CarouselPlugin types in carousel component

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -3,9 +3,11 @@
 import * as React from "react";
 import useEmblaCarousel, {
   type UseEmblaCarouselType as CarouselApi,
-  type EmblaOptionsType as CarouselOptions,
-  type EmblaPluginType as CarouselPlugin,
 } from "embla-carousel-react";
+import EmblaOptionsType from "embla-carousel-react/components/EmblaOptionsType";
+import EmblaPluginType from "embla-carousel-react/components/EmblaPluginType";
+type CarouselOptions = EmblaOptionsType;
+type CarouselPlugin = EmblaPluginType;
 import { ArrowLeft, ArrowRight } from "lucide-react";
 
 import { cn } from "@/lib/utils";
